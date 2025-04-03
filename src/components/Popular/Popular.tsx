0bae972b8c53a5b32d1517c3b1c55fa51f2904d9
hunor64 <nhunor06@gmail.com>
2025-04-03 10:41:42 +0200

    Worp

--- src/components/Popular/Popular.tsx
+++ src/components/Popular/Popular.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react"
 import "./popular.css"
+import PopularCard from "../popularCard/PopularCard"
+
+export type PopularProduct = {
+  name:string,
+  price:string,
+  image:string,
+  description:string
+}
 
 const Popular = () => {
-  {/*Fetcheld be a popular.json-ból az adatokat és tárold le egy állapotváltozóban*/}
 
+  const [products, setProducts] = useState<PopularProduct[]>([])
+
+  {/*Fetcheld be a popular.json-ból az adatokat és tárold le egy állapotváltozóban*/}
+  fetch("popular.json")
+  .then((response) => response.json())
+  .then((data) => {
+    setProducts(data)
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error)
+  })
   {/*Hozz létre egy popularcard komponenst és a lementett adatokat ezen keresztül jelenítsd meg*/}
   return (
     <div>
-
+      {products.map(product => (
+        <PopularCard {...product}/>
+        ))}
     </div>
   )
 }
--- src/components/popularCard/PopularCard.tsx
+++ src/components/popularCard/PopularCard.tsx
@@ -1,9 +1,15 @@
+import { PopularProduct } from "../Popular/Popular"
 import "./popularcard.css"
 
-const PopularCard = () => {
+const PopularCard = (product:PopularProduct) => {
   return (
     <div>
-
+      <div className="popular__card">
+        <img className="popular__image" src={product.image} alt={product.name} />
+        <h3 className="popular__name">{product.name}</h3>
+        <p className="popular__price">{product.price}</p>
+        <p className="popular__description">{product.description}</p>
+      </div>
     </div>
   )
 }
